e0ebd78afb0d3a0c5f4cb312e2fe1f434578e2cb
mahanandhan <mahanandhanmanish@gmail.com>
2025-01-11 11:46:20 +0530

    all are commited

--- server.js
+++ server.js
@@ -33,6 +33,29 @@ Example Responses:
 Add the required logic below to complete the API.
 */
 
+app.get("/assistant/greet", (req, res)=>{
+  let name = req.query.name;
+  let date = new Date().getDay();
+
+  let obj = {
+    welcomeMessage : `Hello, ${name}! Welcome to our assistant app!`,
+  };
+
+  if(date == 1) {
+    obj.dayMessage = "Happy Monday! Start your week with energy!";
+  }
+  else if(date == 5) {
+    obj.dayMessage = "It's Friday! The weekend is near!";
+  } else {
+    obj.dayMessage = "Have a wonderful day!";
+  }
+  return res.send(obj);
+})
+
+app.get("/", function(req, res){
+  return res.send('<h1>Hello World</h1>')
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Virtual Assistant API is running on http://localhost:${PORT}`);
